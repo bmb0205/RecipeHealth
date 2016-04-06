26648ceca2a33c29ee82eb71d3234276fafe78a2
bmb0205 <bburciag@uncc.edu>
2016-04-06 11:02:50 -0400

    finished joining files

--- src/main/java/burciaga/projects/recipehealth/JoinFiles.java
+++ src/main/java/burciaga/projects/recipehealth/JoinFiles.java
@@ -10,6 +10,8 @@ import java.util.*;
 
 /**
  * Created by bmb0205 on 3/31/16.
+ * Parses and joins two delimited files.
+ * Called by ParseCSV.java
  */
 public class JoinFiles {
 
@@ -24,11 +26,6 @@ public class JoinFiles {
         this.joinedFileList = joinedFileList;
         this.file1 = joinedFileList.get(0);
         this.file2 = joinedFileList.get(1);
-    }
-
-    private List<String> getJoinedFileList() {
-
-        return this.joinedFileList;
     }
 
     private String getFile1() {
@@ -46,33 +43,34 @@ public class JoinFiles {
         return new BufferedReader(new InputStreamReader(new FileInputStream(inFileName), "UTF-8"));
     }
 
+    // Parses sets of two pipe-delimited files and merges them together by writing to an outfile
     public void parseFiles() throws Exception {
 
-        // SR_NUT_DATA.txt.out or FLAV_DAT.txt.out
+        System.out.println(file1 + "   " + file2);
+
+        // SR_NUTR_DEF.txt.out or FL_FLAV_NUTR_DEF.txt.out
         String fileString1 = getFile1();
         CsvParser dataParser1 = getFileParser1();
         dataParser1.beginParsing(getReader(fileString1));
 
-        // SR_NUTR_DEF.txt.out or FLAV_NUTR_DEF.txt.out
+        // SR_NUT_DATA.txt.out or FL_FLAV_DAT.txt.out
         String fileString2 = getFile2();
         CsvParser dataParser2 = getFileParser2();
         dataParser2.beginParsing(getReader(fileString2));
 
         CsvWriter csvWriter = getCsvWriter();
 
-        System.out.println("beep");
         String[] row;
         SortedSetMultimap<String, String> nutrMap = TreeMultimap.create();
-        while ((row = dataParser1.parseNext()) != null) { // SR_NUT_DATA.txt.out or FLAV_DAV.txt.out
+        while ((row = dataParser1.parseNext()) != null) { // SR_NUTR_DEF.txt.out or FLAV_NUTR_DEF.txt.out
             String Nutr_No = row[0];
             for ( int i = 1; i <= row.length - 1; i++) {
                 nutrMap.put(Nutr_No, row[i]);
             }
         }
-        System.out.println("boooop");
 
         String[] dataRow;
-        while ((dataRow = dataParser2.parseNext()) != null) { // SR_NUTR_DEF.txt.out or FLAV_NUTR_DEF.txt.out
+        while ((dataRow = dataParser2.parseNext()) != null) {  // SR_NUT_DATA.txt.out or FLAV_DAV.txt.out
             List<String> dataList = new ArrayList<>();
             dataList.addAll(Arrays.asList(dataRow).subList(0, dataRow.length));
             SortedSet<String> myAttributes = nutrMap.get(dataRow[1]);
@@ -85,34 +83,32 @@ public class JoinFiles {
         csvWriter.close();
     }
 
-    public CsvWriter getCsvWriter() throws IOException {
+    // Returns new CsvWriter after specifying CsvWriterSettings and outfile to write to depending on file inut
+    private CsvWriter getCsvWriter() throws IOException {
         CsvWriterSettings writerSettings = new CsvWriterSettings();
         String outFileString;
         if (this.file1.endsWith("FL_NUTR_DEF.txt.out")) {
             writerSettings.setHeaders("NDB_No", "Nutr_No", "Flav_Val", "CC", "Description", "Flav_Class", "Units");
-            outFileString = "/Users/bburciag/BiSD/KnowledgeBase/Sources/USDA/Flavonoid/FL_MERGED.txt.out";
+            outFileString = "/home/bmb0205/BiSD/KnowledgeBase/Sources/USDA/Flavonoid/FL_MERGED.txt.out";
         } else {
-            writerSettings.setHeaders("NDB_No", "Nutr_No", "Nutr_Val", "Units", "NutrDesc");
-            outFileString = "/Users/bburciag/BiSD/KnowledgeBase/Sources/USDA/StandardReference/SR_MERGED.txt.out";
+            writerSettings.setHeaders("Nutr_No", "Units", "NutrDesc");
+            outFileString = "/home/bmb0205/BiSD/KnowledgeBase/Sources/USDA/StandardReference/SR_MERGED.txt.out";
         }
-        System.out.println(this.file1);
-        System.out.println(this.file2);
-        System.out.println(outFileString);
         writerSettings.getFormat().setDelimiter('|');
         OutputStreamWriter outStreamWriter = new OutputStreamWriter(new FileOutputStream(new File(outFileString)));
         return new CsvWriter(outStreamWriter, writerSettings);
     }
 
-    public CsvParser getFileParser1() throws IOException { // SR_NUT_DATA.txt.out or FLAV_DAV.txt.out
+    // Returns new CsvParser with specified parser settings based on file input
+    private CsvParser getFileParser1() throws IOException { // SR_NUTR_DEF.txt.out or FL_NUTR_DEF.txt.out
 
         CsvParserSettings parserSettings1 = new CsvParserSettings();
-        if (this.file1.endsWith("FL_FLAV_DAT.txt.out")) {
-            parserSettings1.setHeaders("NDB_No", "Nutr_No", "Flav_Val", "CC");
-            parserSettings1.selectFields("NDB_No", "Nutr_No", "Flav_Val", "CC");
-
-        } else { //
-            parserSettings1.setHeaders("NDB_No", "Nutr_No", "Nutr_Val");
-            parserSettings1.selectFields("NDB_No", "Nutr_No", "Nutr_Val");
+        if (this.file1.endsWith("SR_NUTR_DEF.txt.out")) {
+            parserSettings1.setHeaders("Nutr_No", "Units", "NutrDesc");
+            parserSettings1.selectFields("Nutr_No", "Units", "NutrDesc");
+        } else { // FL_NUTR_DEF.txt.out
+            parserSettings1.setHeaders("Nutr_No", "Description", "Flav_Class", "Units");
+            parserSettings1.selectFields("Nutr_No", "Description", "Flav_Class", "Units");
         }
         parserSettings1.setNullValue("");
         CsvFormat parserFormat = parserSettings1.getFormat();
@@ -120,16 +116,18 @@ public class JoinFiles {
         return new CsvParser(parserSettings1);
     }
 
-    public CsvParser getFileParser2() { // SR_NUTR_DEF.txt.out or FLAV_NUTR_DEF.txt.out
+    // Returns new CsvParser with specified parser settings based on file input
+    public CsvParser getFileParser2() { // SR_NUT_DATA.txt.out or FL_FLAV_DAT.txt.out
 
         CsvParserSettings parserSettings2 = new CsvParserSettings();
-//        System.out.println(this.file2);
-        if (this.file2.endsWith("SR_NUTR_DEF.txt.out")) {
-            parserSettings2.setHeaders("Nutr_No", "Units", "NutrDesc");
-            parserSettings2.selectFields("Nutr_No", "Units", "NutrDesc");
-        } else { // NUTR_DEF.txt.out
-            parserSettings2.setHeaders("Nutr_No", "Description", "Flav_Class", "Units");
-            parserSettings2.selectFields("Nutr_No", "Description", "Flav_Class", "Units");
+        if (this.file2.endsWith("SR_NUT_DATA.txt.out")) {
+            parserSettings2.setHeaders("NDB_No", "Nutr_No", "Nutr_Val");
+            parserSettings2.selectFields("NDB_No", "Nutr_No", "Nutr_Val");
+
+        } else { // FL_FLAV_DAT.txt.out
+            parserSettings2.setHeaders("NDB_No", "Nutr_No", "Flav_Val", "CC");
+            parserSettings2.selectFields("NDB_No", "Nutr_No", "Flav_Val", "CC");
+
         }
         parserSettings2.setNullValue("");
         CsvFormat parserFormat = parserSettings2.getFormat();
--- src/main/java/burciaga/projects/recipehealth/ParseCSV.java
+++ src/main/java/burciaga/projects/recipehealth/ParseCSV.java
@@ -13,13 +13,14 @@ import com.univocity.parsers.csv.CsvParserSettings;
  * ParseCSV class Uses UniVocity Parser CsvWriter and CsvParser to parse
  * through files of subdirectories in the top level source root directory.
  * main() lists subdirs from top directory, and iterates files within them according to respective class
- * within this package.
+ * within this package. Renames the files for good convention and also joins selected files into
+ * one csv to lower number of tables in future PostgreSQL database
  */
 public class ParseCSV {
 
+    // Adds FL_ or SR_ prefix to file names for easier identification and consistency
     public static void RenameFiles(File subDir) {
         File[] files = subDir.listFiles();
-        System.out.println(Arrays.toString(files));
         Path source = FileSystems.getDefault().getPath(subDir.toString());
         String sourceType = null;
         if (source.toString().endsWith("Flavonoid")) {
@@ -31,11 +32,10 @@ public class ParseCSV {
             Path oldFilePath = FileSystems.getDefault().getPath(file.toString());
             Path newFilePath = FileSystems.getDefault().getPath(subDir.toString() + sourceType + file.getName());
             File newFile = newFilePath.toFile();
-            if(newFile.exists() || newFile.exists()) {
-                System.out.println("Exists!!!" + newFile.toString());
+            if(file.getName().startsWith("SR_") || file.getName().startsWith("FL_")) {
                 continue;
             } else {
-                System.out.println("Does not exist!! Make it!! " + newFile.toString());
+                System.out.println("Creating outfile: " + newFile.toString());
                 try {
                     Files.move(oldFilePath, newFilePath);
                 } catch (IOException e) {
@@ -47,7 +47,7 @@ public class ParseCSV {
 
     public static void main(String[] args) throws Exception {
 
-        File[] subDirs = new File("/Users/bburciag/BiSD/KnowledgeBase/Sources/USDA").listFiles();
+        File[] subDirs = new File("/home/bmb0205/BiSD/KnowledgeBase/Sources/USDA").listFiles();
 
         final Long startTime = System.currentTimeMillis();
 
@@ -55,7 +55,6 @@ public class ParseCSV {
         if (subDirs != null) {
             for (File subDir : subDirs) {
                 String subDirName = subDir.toString().substring(subDir.toString().lastIndexOf('/') + 1);
-
                 if (subDirName.equals("Flavonoid")) {
                     RenameFiles(subDir);
                     File[] flavFiles = subDir.listFiles();
@@ -84,12 +83,11 @@ public class ParseCSV {
 
                     } finally {
                         List<String> joinedFileList = new ArrayList<String>();
-                        String flavFile1 = "/Users/bburciag/BiSD/KnowledgeBase/Sources/USDA/Flavonoid/FL_NUTR_DEF.txt.out";
-                        String flavFile2 = "/Users/bburciag/BiSD/KnowledgeBase/Sources/USDA/Flavonoid/FL_FLAV_DAT.txt.out";
+                        String flavFile1 = "/home/bmb0205/BiSD/KnowledgeBase/Sources/USDA/Flavonoid/FL_NUTR_DEF.txt.out";
+                        String flavFile2 = "/home/bmb0205/BiSD/KnowledgeBase/Sources/USDA/Flavonoid/FL_FLAV_DAT.txt.out";
                         joinedFileList.add(flavFile1);
                         joinedFileList.add(flavFile2);
                         JoinFiles joinObject = new JoinFiles(joinedFileList);
-                        System.out.println(joinedFileList.toString());
                         joinObject.parseFiles();
                     }
 
@@ -121,8 +119,8 @@ public class ParseCSV {
                     } finally {
                         List<String> joinedFileList = new ArrayList<String>();
 
-                        String srFile1 = "/Users/bburciag/BiSD/KnowledgeBase/Sources/USDA/StandardReference/SR_NUTR_DEF.txt.out";
-                        String srFile2 = "/Users/bburciag/BiSD/KnowledgeBase/Sources/USDA/StandardReference/SR_NUT_DATA.txt.out";
+                        String srFile1 = "/home/bmb0205/BiSD/KnowledgeBase/Sources/USDA/StandardReference/SR_NUTR_DEF.txt.out";
+                        String srFile2 = "/home/bmb0205/BiSD/KnowledgeBase/Sources/USDA/StandardReference/SR_NUT_DATA.txt.out";
                         joinedFileList.add(srFile1);
                         joinedFileList.add(srFile2);
                         JoinFiles joinObject = new JoinFiles(joinedFileList);
