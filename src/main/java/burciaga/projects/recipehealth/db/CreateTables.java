f466ff464147d1ea03509c20403cd9bf4399f34f
Brandon Burciaga <bburciag@uncc.edu>
2016-04-13 17:21:15 -0400

    runs psql process to create tables and import delim data using SQL

---
+++ src/main/java/burciaga/projects/recipehealth/db/CreateTables.java
@@ -0,0 +1,38 @@
+package burciaga.projects.recipehealth.db;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Class runs external process using Runtime.getRuntime().exec()
+ * External processes is SQL script run from psql command line.
+ * SQL script create_and_import.sql creates tables and imports delimited data into them.
+ */
+public class CreateTables {
+
+
+    public static void main(String[] args) throws Exception {
+
+        try {
+            Process extProcess = Runtime.getRuntime().exec(
+                    "psql -U bburciag -d recipehealth -f /Users/bburciag/Resume/Java/RecipeHealth/src/main/SQL/create_and_import.sql");
+            BufferedReader inReader = new BufferedReader(new InputStreamReader(extProcess.getInputStream()));
+            BufferedReader errReader = new BufferedReader(new InputStreamReader(extProcess.getErrorStream()));
+            String line;
+            while ((line = inReader.readLine()) != null) {
+                System.out.println(line);
+            }
+            inReader.close();
+            while ((line = errReader.readLine()) != null) {
+                System.out.println(line);
+            }
+            extProcess.waitFor();
+            System.out.println("Done!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
