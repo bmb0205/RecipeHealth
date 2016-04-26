49acc2b75fbbec7266fbaf368e44f60865a8cd4c
bmb0205 <bburciag@uncc.edu>
2016-04-26 11:11:37 -0400

    fixed mesh/pubmed outfiles

--- src/main/java/burciaga/projects/recipehealth/xml/XMLParser.java
+++ src/main/java/burciaga/projects/recipehealth/xml/XMLParser.java
@@ -10,6 +10,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.util.List;
+import java.util.UUID;
 
 
 /**
@@ -22,27 +23,26 @@ public class XMLParser {
         SAXParser saxParser = saxParserFactory.newSAXParser();
         SAXHandler saxHandler = new SAXHandler();
         saxParser.parse(new File(
-                "/home/bmb0205/BiSD/KnowledgeBase/Sources/PubMed/inflammation_flavonoid_pubmed.xml"), saxHandler);
+                "/home/bmb0205/BiSD/KnowledgeBase/Sources/PubmedAdvancedSearch/flavonoid_inflammation_human.xml"), saxHandler);
         List<PubmedArticle> articleList = saxHandler.getArticleList();
 
         BufferedWriter meshTermWriter = new BufferedWriter(new FileWriter(
-                new File("/home/bmb0205/BiSD/KnowledgeBase/Sources/PubMed/pubmed_meshTerms.txt.out")));
+                new File("/home/bmb0205/BiSD/KnowledgeBase/Sources/PubmedAdvancedSearch/pubmed_meshTerms.out")));
         BufferedWriter pubmedInfoWriter = new BufferedWriter(new FileWriter(
-                new File("/home/bmb0205/BiSD/KnowledgeBase/Sources/PubMed/pubmed_info.txt.out")));
+                new File("/home/bmb0205/BiSD/KnowledgeBase/Sources/PubmedAdvancedSearch/pubmed_info.out")));
 
-        meshTermWriter.write("PMID|MeSH_Term\n");
-        pubmedInfoWriter.write("PMID|Title|Abstract\n");
+//        meshTermWriter.write("PMID|MeSH_Term\n");
+//        pubmedInfoWriter.write("PMID|Title|Abstract\n");
         for (PubmedArticle article : articleList) {
             if (article.getTitle().startsWith("Reply:")) {
                 continue;
             }
             pubmedInfoWriter.write(article.getPMID() + "|" + article.getTitle() + "|" + article.getAbs() + "\n");
             for (String meshTerm : article.getMeshSet()) {
-                meshTermWriter.write(article.getPMID() + "|" + meshTerm + "\n");
+                meshTermWriter.write( meshTerm + "|" + article.getPMID() + "|" + UUID.randomUUID() + "\n");
             }
         }
         meshTermWriter.close();
         pubmedInfoWriter.close();
-
     }
 }
