016caca9a9213ecd05b466f3b34837a6e57c24d9
bmb0205 <bburciag@uncc.edu>
2016-05-04 12:47:46 -0400

    for recipe url object

---
+++ src/main/java/burciaga/projects/recipehealth/recipe/Recipe.java
@@ -0,0 +1,19 @@
+package burciaga.projects.recipehealth.recipe;
+
+/**
+ * Created by bmb0205 on 5/1/16.
+ */
+public class Recipe {
+
+    private String url;
+
+    public String getUrl() {
+        return this.url;
+    }
+
+    @Override
+    public String toString() {
+        return this.url;
+    }
+
+}
