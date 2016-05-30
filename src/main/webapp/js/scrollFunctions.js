aa2331b00ce4d7c665e416444d6efeed0840a525
bmb0205 <bburciag@uncc.edu>
2016-05-29 20:59:32 -0400

    removed javascript code from HTML - sep of concerns

---
+++ src/main/webapp/js/scrollFunctions.js
@@ -0,0 +1,73 @@
+/*
+Created by bmb0205 on 5/29/16.
+JavaScript functions called by ../index.html and ../url.html
+Uses jQuery and AJAX in scrolling functions and GET/POSTS requests
+*/
+
+// auto-scrolls to 'Why RecipeHealth?' div
+function scrollToWhy() {
+    $('html, body').animate({
+        scrollTop: $('.why').offset().top
+    }, 1000);
+    return false;
+}
+
+// auto-scrolls to 'Why RecipeHealth?' div
+function scrollToFlav() {
+    $('html, body').animate({
+        scrollTop: $('.flav').offset().top
+    }, 1000);
+    return false;
+}
+
+// auto-scrolls to 'Isoflavones' div
+function scrollToIso() {
+    $('html, body').animate({
+        scrollTop: $('.iso').offset().top
+    }, 1000);
+    return false;
+}
+
+// auto-scrolls to 'Proanthocyanidins' div
+function scrollToPro() {
+    $('html, body').animate({
+        scrollTop: $('.pro').offset().top
+    }, 1000);
+    return false;
+}
+
+// auto-scrolls to 'About Us' div
+function scrollToAbout() {
+    $('html, body').animate({
+        scrollTop: $('.about').offset().top
+    }, 1000);
+    return false;
+}
+
+/*
+AJAX POST request connecting to GetRecipeURL.do Java servlet which handles request
+and queries PostgreSQL database before writing results to a table
+*/
+function getURL() {
+    var $urlString = $('.txtUrl').val();
+    var params = {url: $urlString};
+    var jsonDataString = JSON.stringify(params);
+    var $jumboContainer = $('.jumboContainer');
+    $.ajax({
+        type: "POST",
+        url: "GetRecipeURL.do",
+        contentType: "application/json",
+        dataType: "text",
+        data: jsonDataString,
+        success: function(data) {
+            $jumboContainer.empty();
+            $jumboContainer.append(data);
+            $jumboContainer.css("font-size", "18");
+        },
+        error: function(data) {
+            $jumboContainer.clear();
+            $jumboContainer.append("Could not load the data!")
+        }
+    });
+    return false;
+}
